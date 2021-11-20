cd003314cff7e2e7f88ce4cd02cc03ad1e181825
Brian Duong <brianminhduong@gmail.com>
2021-11-20 08:34:35 +0000

    File that accepts a breedname and searches for description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,13 @@
 const request = require('request');
+const breed = process.argv.slice(2);
+const searchQuery = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-request('https://api.thecatapi.com/v1/breeds/search?q=siberian', (error, response, body) => {
-  console.log(typeof body);
-})
+request(searchQuery + breed, (error, response, body) => {
+  if (error) {
+    console.log('Error:', error);
+  } else if (JSON.parse(body).length < 1) {
+    console.log('Breed is not found');
+  } else {
+    console.log(JSON.parse(body)[0]['description']);
+  }
+});
